b24d78f006543e8dcd6b86d535fed88db1c0a953
rishav jha <jharishav763@gmail.com>
2024-11-08 20:10:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// let List = document.getElementById("list");
+// List.style.maxHeight = "0px";
+
+// function random() {
+//     if (List.style.maxHeight = "0px") {
+//         List.style.maxHeight = "300px";
+//     }
+
+//     else {
+//         List.style.maxHeight = "0px";
+//     }
+// }
